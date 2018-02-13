5fcfb932657f80f49951f3d9a65fe4a712b1cc57
tosyama <tos-yama@sa2.so-net.ne.jp>
2018-02-14 08:27:47 +0900

    Fixed function syntax bug. (#19)

---
+++ test/algorithmTest.cpp
@@ -0,0 +1,11 @@
+#include "testBase.h"
+
+TEST_CASE("basic algorithm code example.", "[algorithm]")
+{
+	string testcode;
+
+	testcode = "100_qsort";
+	REQUIRE(build(testcode) == "success");
+	REQUIRE(exec(testcode) == "before: 0 4 8 3 7 2 6 1 5 0\n"
+							"after: 0 0 1 2 3 4 5 6 7 8");
+}
