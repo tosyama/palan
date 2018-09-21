5fb3b1fd9b18cbf76ea1e5a0f737d6e8893635c0
YAMAGUCHI Toshinobu <tos-yama@sa2.so-net.ne.jp>
2018-09-21 19:10:25 +0900

    Fixed failed test.

--- test/basicTest.cpp
+++ test/basicTest.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Normal case with simple grammer", "[basic]")
 
 	testcode = "000_temp";
 	REQUIRE(build(testcode) == "success");
-	REQUIRE(exec(testcode) == "5 1");
+	REQUIRE(exec(testcode) == "0 1");
 
 	testcode = "002_varint64";
 	REQUIRE(build(testcode) == "success");
